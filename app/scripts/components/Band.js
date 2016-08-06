cffc47c8163178fc51c043b253630c08fb1a3c06
Travis Gorman <thefarbetween@me.com>
2016-08-06 15:49:25 -0500

    return individual bands in search results

--- app/scripts/components/Band.js
+++ app/scripts/components/Band.js
@@ -3,12 +3,12 @@ import React from 'react';
 export default React.createClass({
   submitVote: function () {
 
-  }
+  },
   render: function(){
     return (
       <div className="band">
-        <img src='http://www.fillmurray.com/120/120' />
-        <div className="bandName">Band Name!!</div>
+        <img src={this.props.img} />
+        <div className="bandName">{this.props.bandName}</div>
         <input
           type="button"
           value="vote"
--- app/scripts/components/SearchResults.js
+++ app/scripts/components/SearchResults.js
@@ -1,36 +1,56 @@
 import React from 'react';
 import $ from 'jquery';
+import Band from './Band';
+
 
 export default React.createClass({
+  getInitialState: function () {
+    return {
+        results: []
+    }
+  },
   submitSearch: function () {
     let searchQuery = this.refs.search.value;
+
     $.ajax({
       url: `https://api.spotify.com/v1/search`,
       data: {q: searchQuery, type: 'artist'},
-      success: function (data) {
+      success: (data) => {
         let results = data.artists.items;
-        console.log(results);
+        this.setState({
+          results: results
+        });
       }
-    })
-  },
-  componentDidMount: function(){
-
+    });
   },
   render: function () {
+    let mySearchResults = this.state.results.map( (result, i) => {
+      if (result.images.length === 0) {
+        result.images.push({ url:'https://s8.postimg.org/a8vgc0k5x/no_Image.png'
+        });
+      }
+      return (
+          <Band
+            key={i}
+            img={result.images[0].url}
+            bandName={result.name}
+           />
+       )
+    });
     return (
       <div>
         <input
           type="text"
           placeholder="search for artist..."
           ref="search"
-          />
+        />
         <input
           type="button"
           value="submit"
           onClick={this.submitSearch}
-          />
+        />
         <div className="searchResults">
-          search results go here..
+        {mySearchResults}
         </div>
       </div>
     )
